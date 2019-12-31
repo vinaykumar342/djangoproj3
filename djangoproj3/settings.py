936c1c55a1501e308dae23c68a69c3f53d3b32aa
vinaykumar342 <45268796+vinaykumar342@users.noreply.github.com>
2019-12-31 12:53:02 +0530

    Update settings.py

--- djangoproj3/settings.py
+++ djangoproj3/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'r+#$)oq4*nh#2o9&bpiy+go**1rlv1(13t_o6kxf$%4gp2*$j)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
